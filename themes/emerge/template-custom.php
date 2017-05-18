289b24f6a3c31f79a5714d1e85ea2335fcf3dec6
cbast16 <calvin.bast@gmail.com>
2017-05-18 13:28:46 -0500

    Some updated content and css

--- themes/emerge/template-custom.php
+++ themes/emerge/template-custom.php
@@ -12,15 +12,42 @@ $logo = get_field('theme_logo', 'options');
 
 <header class="component component-header">
   <img src="<?php echo $logo['url']; ?>" alt="<?php echo $logo['alt']; ?>" class="logo">
-  <h1>Header component</h1>
+
+
+    <nav>
+        <hr width="700">
+        <ul>
+            <li><a href="#">WHAT WE DO</a></li>
+            <li><a href="#">WHO WE ARE</a></li>
+            <li><a href="#">CONTACT</a></li>
+        </ul>
+    </nav>
 </header>
 
 <section class="component component-marquee">
-  <h1>Marquee component</h1>
+    <ul class=“carousel-container”>
+         <li class=“carousel-img”>
+            <img src=“../img.jpg” alt=“”>
+          </li>
+          <li class=“carousel-img”>
+            <img src=“../img.jpg” alt=“”>
+          </li>
+          <li class=“carousel-img”>
+            <img src=“../img.jpg” alt=“”>
+          </li>
+    </ul>
 </section>
 
 <section class="component component-what-we-do">
-  <h1>What we do component</h1>
+    <div class="containter">
+        <div class="row">
+            <div class="col-md-12 text-left">
+              <h1 class="color-1">WHAT WE DO</h1>
+                <h4>OUR PROCESS</h4>
+                <p></p>
+            </div>
+        </div>
+    </div>
 </section>
 
 <section class="component component-partners">
@@ -29,6 +56,32 @@ $logo = get_field('theme_logo', 'options');
 
 <section class="component component-who-we-are">
   <h1>Who we are component</h1>
+    <div class="containter">
+        <div class="row ">
+            <div class="col-md-3"></div>
+            <div class="col-md-3">
+                <img src="assets/imgs/person1.png" class="who_img">
+            </div>
+            <div class="col-md-3">
+                <img src="assets/imgs/person1.png" class="who_img">
+            </div>
+            <div class="col-md-3">
+                <img src="/assets/imgs/person1.png" class="who_img">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-3"></div>
+            <div class="col-md-3">
+                <img src="assets/imgs/person1.png" class="who_img">
+            </div>
+            <div class="col-md-3">
+                <img src="assets/imgs/person1.png" class="who_img">
+            </div>
+            <div class="col-md-3">
+                <img src="assets/imgs/person1.png" class="who_img">
+            </div>
+        </div>
+    </div>
 </section>
 
 <section class="component component-get-connected">
